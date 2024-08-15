dbb8e2e4397694a7ed64af0fea5873db859cae66
conaffetto <167137372+conaffetto@users.noreply.github.com>
2024-08-15 19:13:47 +0300

    removed search configs

--- init.lua
+++ init.lua
@@ -104,9 +104,6 @@ vim.opt.showmode = false
 vim.opt.breakindent = true
 
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
 
 -- Keep signcolumn on by default
 vim.opt.signcolumn = 'yes'
@@ -136,9 +133,6 @@ vim.opt.cursorline = true
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
