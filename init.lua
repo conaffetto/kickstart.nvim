0528aa860c6ca78d838b5f190b9d7132af9ba6e7
conaffetto <167137372+conaffetto@users.noreply.github.com>
2024-08-20 20:17:23 +0300

    removed some lines which I incorporated to my personal configs

--- init.lua
+++ init.lua
@@ -142,16 +142,6 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
 
 -- [[ Install `lazy.nvim` plugin manager ]] - *YOYO NOTE: THE FOLLOWING BLOCK IS ALREADY IN MY init.lua ! 
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
